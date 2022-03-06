9ed46432fb44c9e4b840e34f390221920275f77d
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    psyche: Update to Boot Control HAL v1.2

--- device.mk
+++ device.mk
@@ -62,9 +62,9 @@ PRODUCT_COPY_FILES += \
 
 # A/B
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
