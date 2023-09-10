c0dc31cac784b14ca92418b51220c78f07f0c504
lamjiidii1 <lamjiidii1@gmail.com>
2023-09-10 21:37:23 +0100

    psyche: Fix Typo!

--- device.mk
+++ device.mk
@@ -430,7 +430,7 @@ PRODUCT_PACKAGES += \
     sensors.xiaomi
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
