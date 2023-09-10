e4af90152faaf25e77c413b86e7eab917b880f80
lamjiidii1 <lamjiidii1@gmail.com>
2023-09-10 16:04:05 +0100

    Revert "psyche: Build lineage health HAL" * people saying that it's not safe for our SoC.
    This reverts commit 731094c73cb9c008d481d7cffc98a4e62d3f4e66.

--- device.mk
+++ device.mk
@@ -290,10 +290,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
